b10e4364a4dea4426dfc26b5c2dc6207db7ecd7f
randallka-ga <randallkaem@gmail.com>
2022-12-01 12:02:20 -0700

    nav bar add, index redirect

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/flights');
+var flightsRouter = require('./routes/flights');
 const destinationsRouter = require('./routes/destinations');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/flights', usersRouter);
+app.use('/flights', flightsRouter);
 app.use('/', destinationsRouter);
 
 // catch 404 and forward to error handler
